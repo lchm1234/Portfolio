afe39a406f373da282031b9d93e7cc7069e33d38
huLee <guddnr6123@naver.com>
2023-08-28 17:41:28 +0900

    주석 추가

--- 1_1/Optimizer_Comparison/optimizer_Comparison.py
+++ 1_1/Optimizer_Comparison/optimizer_Comparison.py
@@ -10,11 +10,11 @@ from common.optimizer import *
 def f(x, y):
     return x**2 / 20.0 + y**2
 
-
+# delta f
 def df(x, y):
     return x / 10.0, 2.0*y
 
-
+# 초기위치 설정
 init_pos = (-7.0, 2.0)
 params = {}
 params['x'], params['y'] = init_pos[0], init_pos[1]
